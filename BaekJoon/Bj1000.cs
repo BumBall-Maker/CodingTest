91ae2cf982f83cf9d13e3a660d96cce30ef4c057
BumBall <bigwatermoon@naver.com>
2023-05-02 08:12:23 +0900

    230502_01
    깃헙 체크

---
+++ BaekJoon/Bj1000.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Bj1000
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine(int a);
+        }
+    }
+}
---
+++ BaekJoon/Bj2557.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Bj2557
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
